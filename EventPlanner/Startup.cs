5dacdb4cd90faaca2006e5ada9289a53ab1bf044
unknown <aaronedelman@yahoo.com>
2018-01-12 10:39:50 -0600

    added attendee and promoter roles.  changed login to UserName/PW

--- EventPlanner/Startup.cs
+++ EventPlanner/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using EventPlanner.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(EventPlanner.Startup))]
@@ -9,6 +12,36 @@ namespace EventPlanner
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login   
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+
+
+            // creating Creating Promoter role    
+            if (!roleManager.RoleExists("Promoter"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Promoter";
+                roleManager.Create(role);
+            }
+            // Creating Attendee role    
+            if (!roleManager.RoleExists("Attendee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Attendee";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
