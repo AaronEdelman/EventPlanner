00601f0a10ad9d9db231861bc7ac810683bed039
unknown <aaronedelman@yahoo.com>
2018-01-12 16:30:44 -0600

    Added attendee and Promoter classes/default views

---
+++ EventPlanner/Controllers/AttendeeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventPlanner.Models;
+
+namespace EventPlanner.Controllers
+{
+    public class AttendeeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Attendee
+        public ActionResult Index()
+        {
+            var userToGroups = db.UserToGroups.Include(u => u.Group).Include(u => u.User);
+            return View(userToGroups.ToList());
+        }
+
+        // GET: Attendee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserToGroup userToGroup = db.UserToGroups.Find(id);
+            if (userToGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userToGroup);
+        }
+
+        // GET: Attendee/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupId = new SelectList(db.Groups, "Id", "Name");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Attendee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,GroupId")] UserToGroup userToGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserToGroups.Add(userToGroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupId = new SelectList(db.Groups, "Id", "Name", userToGroup.GroupId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userToGroup.UserId);
+            return View(userToGroup);
+        }
+
+        // GET: Attendee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserToGroup userToGroup = db.UserToGroups.Find(id);
+            if (userToGroup == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupId = new SelectList(db.Groups, "Id", "Name", userToGroup.GroupId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userToGroup.UserId);
+            return View(userToGroup);
+        }
+
+        // POST: Attendee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,GroupId")] UserToGroup userToGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userToGroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupId = new SelectList(db.Groups, "Id", "Name", userToGroup.GroupId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userToGroup.UserId);
+            return View(userToGroup);
+        }
+
+        // GET: Attendee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserToGroup userToGroup = db.UserToGroups.Find(id);
+            if (userToGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userToGroup);
+        }
+
+        // POST: Attendee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserToGroup userToGroup = db.UserToGroups.Find(id);
+            db.UserToGroups.Remove(userToGroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EventPlanner/Controllers/PromoterController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventPlanner.Models;
+
+namespace EventPlanner.Controllers
+{
+    public class PromoterController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Promoter
+        public ActionResult Index()
+        {
+            var entertainments = db.Entertainments.Include(e => e.Event).Include(e => e.Venue);
+            return View(entertainments.ToList());
+        }
+
+        // GET: Promoter/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entertainment entertainment = db.Entertainments.Find(id);
+            if (entertainment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entertainment);
+        }
+
+        // GET: Promoter/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventId = new SelectList(db.Events, "Id", "Name");
+            ViewBag.VenueId = new SelectList(db.Venues, "Id", "Name");
+            return View();
+        }
+
+        // POST: Promoter/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,StartTime,EndTime,StartDate,EndDate,Restriction,VenueId,EventId")] Entertainment entertainment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entertainments.Add(entertainment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EventId = new SelectList(db.Events, "Id", "Name", entertainment.EventId);
+            ViewBag.VenueId = new SelectList(db.Venues, "Id", "Name", entertainment.VenueId);
+            return View(entertainment);
+        }
+
+        // GET: Promoter/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entertainment entertainment = db.Entertainments.Find(id);
+            if (entertainment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventId = new SelectList(db.Events, "Id", "Name", entertainment.EventId);
+            ViewBag.VenueId = new SelectList(db.Venues, "Id", "Name", entertainment.VenueId);
+            return View(entertainment);
+        }
+
+        // POST: Promoter/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,StartTime,EndTime,StartDate,EndDate,Restriction,VenueId,EventId")] Entertainment entertainment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entertainment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventId = new SelectList(db.Events, "Id", "Name", entertainment.EventId);
+            ViewBag.VenueId = new SelectList(db.Venues, "Id", "Name", entertainment.VenueId);
+            return View(entertainment);
+        }
+
+        // GET: Promoter/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entertainment entertainment = db.Entertainments.Find(id);
+            if (entertainment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entertainment);
+        }
+
+        // POST: Promoter/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Entertainment entertainment = db.Entertainments.Find(id);
+            db.Entertainments.Remove(entertainment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
