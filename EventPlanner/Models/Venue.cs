13d32b4594e186a0c3336da03d8ad5fbf5929e40
josephmkotvis <josephmkotvis@gmail.com>
2018-01-12 15:49:16 -0600

    Fixed ID in venue

--- EventPlanner/Models/Venue.cs
+++ EventPlanner/Models/Venue.cs
@@ -10,7 +10,7 @@ namespace EventPlanner.Models
     public class Venue
     {
         [Key]
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
