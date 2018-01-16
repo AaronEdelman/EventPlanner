b4b0e36f8b511ce130284c66b4c65dc9a13121bc
unknown <aaronedelman@yahoo.com>
2018-01-16 16:32:58 -0600

    updated view entertainment/venues in promoter, begun work on weather api

--- EventPlanner/Controllers/HomeController.cs
+++ EventPlanner/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using EventPlanner.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,38 +8,15 @@ namespace EventPlanner.Controllers
 {
     public class HomeController : Controller
     {
-        ApplicationDbContext _context;
-        public HomeController()
-        {
-            _context = new ApplicationDbContext();
-        }
         public ActionResult Index()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var UserHomeModel = new HomeViewModel();
-                var currentUserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
-                foreach (ApplicationUser user in _context.Users)
-                {
-                    if (user.Id == currentUserId)
-                    {
-                        UserHomeModel.User = user;
-                        var roles = (from x in _context.Roles where x.Id == currentUserId select x.Name);
-                        foreach(string roleName in roles)
-                        {
-                            UserHomeModel.Role = roleName;
-                        }
-                    }
-                }
-                
-                return View(UserHomeModel);
-            }
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+
             return View();
         }
 
